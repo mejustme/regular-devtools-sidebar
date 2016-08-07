a1710706a6ac7de32b21eeb9f1c2f50557caa5e9
陈钦辉 <hzchenqinhui@corp.netease.com>
2016-08-07 23:55:41 +0800

    @dev update

--- js/background.js
+++ js/background.js
@@ -4,11 +4,11 @@ chrome.runtime.onConnect.addListener(function(connection) {
 
   if (connection.name == "dev2backConnection") {
     dev2backConnection = connection;
-    dev2backConnection.onMessage.addListener(function(message, sender, sendResponse){ // dev -> back
+    dev2backConnection.onMessage.addListener(function (message, sender, sendResponse) { // dev -> back
 
     });
     return;
-
+  }
 
   if (connection.name === "front2backConnection") {
     front2backConnection = connection;
